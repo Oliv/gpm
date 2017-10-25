1d08ebb54f162a9db25c9b4693a01335ef009808
Oliv <jahsoul@gmail.com>
2017-10-25 12:53:25 +0200

    Fix wrong paste

--- tools/extractor.js
+++ tools/extractor.js
@@ -3,8 +3,6 @@ const fs = require('fs');
 
 function extractor(source, target) {
   return new Promise((resolve, reject) => {
-    const writeStream = fs.createWriteStream(target);
-
     const extract = tar.Extract({ path: target })
       .on('error', reject)
       .on('end', resolve)
